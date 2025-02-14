8d904658f6a9022bc3c8a14b5e1e8d3fc4b4caf4
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-14 17:46:24 +0300

    added logger middleware

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
+const { logger } = require('./middlewares/logger');
 
 // MIDDLEWARES
+app.use(logger);
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use('/', express.static(path.join(__dirname, 'public')));
 
 // ROUTES

331bb0e1842926a265795667e268857093427294
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-15 22:05:48 +0300

    configured userRoutes and userControllers and upload middleware for s3 bucket

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const { uploadAvatar, uploadHeader } = require('../middlewares/upload');
 
 router
     .route('/')
@@ -8,5 +9,21 @@ router
     .post(userController.createUser)
     .patch(userController.updateUser)
     .delete(userController.deleteUser);
+
+router
+    .route('/:username/upload_avatar')
+    .post(uploadAvatar, userController.uploadAvatarToUser);
+
+router
+    .route('/:username/upload_header')
+    .post(uploadHeader, userController.uploadHeaderToUser);
+
+router
+    .route('/:username/delete_avatar')
+    .delete(userController.deleteAvatarFromUser);
+
+router
+    .route('/:username/delete_header')
+    .delete(userController.deleteHeaderFromUser);
 
 module.exports = router;

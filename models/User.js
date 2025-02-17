d59772ea714be7dad01965fa50a4653f6b624844
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-17 23:03:10 +0300

    user model refactored

--- models/User.js
+++ models/User.js
@@ -6,42 +6,52 @@ const userSchema = new mongoose.Schema(
             type: String,
             required: true,
         },
+
         password: {
             type: String,
             required: true,
         },
+
         fullname: {
             type: String,
         },
+
         avatar: {
             type: String, // Store the URL of the avatar image
             default: '', // Default to an empty string if no avatar is provided
         },
+
         header_photo: {
             type: String, // Store the URL of the header photo
             default: '', // Default to an empty string if no header photo is provided
         },
+
         verified: {
             type: Boolean,
             default: false,
         },
+
         bio: {
             type: String,
             default: '',
         },
+
         location: {
             type: String,
             default: '',
         },
+
         website: {
             type: String,
             default: '',
         },
+
         followers: {
             type: [mongoose.Schema.Types.ObjectId], // Array of user IDs for followers
             ref: 'User',
             default: [],
         },
+
         following: {
             type: [mongoose.Schema.Types.ObjectId], // Array of user IDs for following
             ref: 'User',

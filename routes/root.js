eceb4ff9a87099afe585e270aa6f39fb501c255e
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-18 22:33:22 +0300

    home page change to getPosts

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
+const postController = require('../controllers/postController');
 
-router.get('^/$|/index(.html)?', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
-});
+router.get('/', postController.getPosts);
 
 module.exports = router;

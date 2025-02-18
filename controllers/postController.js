1c0626d3279df17972dddc7013f7cec44729fc1a
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-18 16:27:57 +0300

    createPost function is implemented to postController

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,14 +1,45 @@
 const User = require('../models/User');
 const Post = require('../models/Post');
-const { deleteFileFromS3 } = require('../utils/s3Helpers');
+const { uploadFilesToGoogleDrive } = require('../utils/googleDriveHelper');
 
 const getPosts = async (req, res) => {
     const posts = await Post.find().lean().populate('UserId', 'username');
     if (!posts?.length) {
-        return res.status(400).json({ message: 'No posts found' });
+        return res.status(404).json({ message: 'No posts found' });
     }
 
     res.status(200).json(posts);
 };
 
-const createPosts = async;
+const createPost = async (req, res) => {
+    const { userId, content } = req.body;
+    const mediaFiles = req.files || [];
+
+    const user = await User.findById(userId).exec();
+    if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+    }
+
+    let mediaUrls = [];
+    if (mediaFiles.length > 0) {
+        for (const file of mediaFiles) {
+            const mediaUrl = await uploadFilesToGoogleDrive(
+                file,
+                process.env.GOOGLE_DRIVE_POSTMEDIA_FOLDERID
+            );
+            mediaUrls.push(mediaUrl);
+        }
+    }
+
+    const post = await Post.create({
+        content,
+        userId,
+        media: mediaUrls,
+    });
+
+    if (post) {
+        return res.status(201).json({ message: 'New note created', post });
+    } else {
+        return res.status(400).json({ message: 'Invalid note data received' });
+    }
+};

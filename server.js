5deb3d7b9daadce137f6e52dff8d63d0d96ebf71
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-18 16:49:32 +0300

    created a route parameter for each post

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 // ROUTES
 app.use('/', require('./routes/root'));
 app.use('/users', require('./routes/userRoutes'));
+app.use('/:username/status/:postId', require('/routes/postRoutes'));
 
 app.all('*', (req, res) => {
     res.status(404);

852a4633f29f0865a4e748668298c46cb5353662
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-20 00:23:55 +0300

    authController and authRoutes implemented

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -4,5 +4,7 @@ const loginLimiter = require('../middlewares/loginLimiter');
 
 router.route('/signup').post();
 router.route('/login').post(loginLimiter);
+router.route('/refresh').get();
+router.route('/logout').post();
 
 module.exports = router;

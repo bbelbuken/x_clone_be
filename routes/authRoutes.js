e40232ef2efaee5f09637c14695eac111a99c23f
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-20 18:12:54 +0300

    we implemented our controller to authRoutes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const loginLimiter = require('../middlewares/loginLimiter');
+const authController = require('../controllers/authController');
 
 router.route('/signup').post();
-router.route('/login').post(loginLimiter);
-router.route('/refresh').get();
-router.route('/logout').post();
+router.route('/login').post(loginLimiter, authController.login);
+router.route('/refresh').get(authController.refresh);
+router.route('/logout').post(authController.logout);
 
 module.exports = router;

5e542fe1bec7d7b6b47259e656a4fb7cc3f30ec5
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-20 22:58:11 +0300

    server has changed to post 8080

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router({ mergeParams: true }); // ! important
 const postController = require('../controllers/postController');
 const { upload } = require('../middlewares/multer'); // Adjust the path if necessary
-const verifyJWT = require('../middlewares/verifyJWT');
-
-router.use(verifyJWT);
 
 router
     .route('/')

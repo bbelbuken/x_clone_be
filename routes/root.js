6260d5d861dd8634cf540fa3ff38c55c1d22c35b
Batuhan Belbüken <bbelbuken@hotmail.com>
2025-02-21 00:15:52 +0300

    configures root route

--- routes/root.js
+++ routes/root.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 const postController = require('../controllers/postController');
 
-router.get('/home', postController.getPosts);
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
 
 module.exports = router;

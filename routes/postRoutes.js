db2f2e598c2da125f5421fbde3df19637ae7d05b
Batuhan Belbüken <bbelbuken@gmail.com>
2025-02-20 23:41:38 +0300

    verifyJWT can not be applied with .use on a dynamic route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -4,16 +4,14 @@ const postController = require('../controllers/postController');
 const { upload } = require('../middlewares/multer'); // Adjust the path if necessary
 const verifyJWT = require('../middlewares/verifyJWT');
 
-router.use(verifyJWT);
-
 router
     .route('/')
-    .get(postController.getPosts)
-    .post(upload.array('mediaFiles', 4), postController.createPost);
+    .get(verifyJWT, postController.getPosts)
+    .post(verifyJWT, upload.array('mediaFiles', 4), postController.createPost);
 
 router
     .route('/status/:postId')
-    .patch(upload.array('mediaFiles', 4), postController.updatePost)
-    .delete(postController.deletePost);
+    .patch(verifyJWT, upload.array('mediaFiles', 4), postController.updatePost)
+    .delete(verifyJWT, postController.deletePost);
 
 module.exports = router;

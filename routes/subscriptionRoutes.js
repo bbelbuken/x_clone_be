1d43950629a6fbb5d0d42783ebdcefe712590d93
Batuhan Belbüken <bbelbuken@gmail.com>
2025-05-15 23:55:55 +0300

    subscription method updated

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -3,5 +3,4 @@ const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
 
 router.route('/').post(subscriptionController.handleSubscription);
-
 module.exports = router;

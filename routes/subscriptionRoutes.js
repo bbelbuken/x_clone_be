ac25ed44c87070115802d34e068e8a7947d6dfea
Batuhan Belbüken <bbelbuken@gmail.com>
2025-05-16 22:56:14 +0300

    improved codebase

--- routes/subscriptionRoutes.js
+++ routes/subscriptionRoutes.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const subscriptionController = require('../controllers/subscriptionController');
 
 router.route('/').post(subscriptionController.handleSubscription);
+
 module.exports = router;
